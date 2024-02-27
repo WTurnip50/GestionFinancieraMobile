b00244b7edda719ae3dbf9f15c6deacaa59b2ead
Israel Garcia Sanchez <isreal2341@outlook.com>
2024-02-27 16:22:49 -0600

    Initial Commit

---
+++ gestion_financiera_mov/bin/gestion_financiera_mov.dart
@@ -0,0 +1,5 @@
+import 'package:gestion_financiera_mov/gestion_financiera_mov.dart' as gestion_financiera_mov;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gestion_financiera_mov.calculate()}!');
+}
---
+++ gestion_financiera_mov/test/gestion_financiera_mov_test.dart
@@ -0,0 +1,8 @@
+import 'package:gestion_financiera_mov/gestion_financiera_mov.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
